2e7d4aef350daca96e576a0856184395c0904515
Dima Romanchenko <romanchenko.d.lightit@gmail.com>
2023-09-20 01:39:06 +0300

    Lab1 task 1 return null if number is null

--- lab1/lab1.kt
+++ lab1/lab1.kt
@@ -3,11 +3,13 @@ import kotlin.math.sqrt
 fun isEven(arg: Int?): String {
         if (arg != null && arg % 2 == 0) {
             return "even"
-        } else {
+        } else if (arg != null && arg % 2 == 1){
             return "odd"
+        } else {
+            return ""
+        }
     }
 
-}
 
 fun sumSimple(): Int {
     val numbers = mutableListOf<Int>()
@@ -55,9 +57,14 @@ fun main() {
     var result = isEven(arg)
     println("$arg is $result")
 
+    arg = null
+    result = isEven(arg)
+    println("$arg is $result")
+
     arg = 11
     result = isEven(arg)
     println("$arg is $result")
+
     val result2 = sumSimple()
     println("Sum of first 5 prime numbers is $result2")
     var test:Int = 5
