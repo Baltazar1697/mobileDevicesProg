c8cf6914e2bcd9cae5dc11f098f883c15c884434
Dima Romanchenko <romanchenko.d.lightit@gmail.com>
2023-09-20 01:33:32 +0300

    Lab1 add alternative to 3d task

--- lab1/lab1.kt
+++ lab1/lab1.kt
@@ -31,25 +31,36 @@ fun sumSimple(): Int {
 }
 
 fun sumOnes(n:Int): Int {
-    var number:Int = 1
-    for (i in 2..n){
-        var temp = "1".repeat(i).toInt()
-        number+=temp
+    var number:Double = 1.0
+
+    // ALTERNATIVE: using cheat-code with repeating strings
+
+    // for (i in 2..n){  
+    //     var temp = "1".repeat(i).toInt()
+    //     number+=temp
+    // }
+
+    for ( i in 1..n){
+        for (j in 0..i){
+            var temp:Double = Math.pow(10.0,j.toDouble())
+            number+=temp
+
+        }
     }
-    return number
+    return number.toInt()
     
 }
 fun main() {
     var arg:Int? = 10
     var result = isEven(arg)
     println("$arg is $result")
-    
+
     arg = 11
     result = isEven(arg)
     println("$arg is $result")
     val result2 = sumSimple()
     println("Sum of first 5 prime numbers is $result2")
-    var test:Int = 6
+    var test:Int = 5
     var result3 = sumOnes(test)
     println("The result of summing 1 $test times will be $result3") 
 }
